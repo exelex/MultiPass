da8e5967e47f262615365c5d2dd3e6b7d8723695
Alexey-frontend <lexer.work@gmail.com>
2021-02-25 20:25:45 +0300

    add viewcontrollers

--- MultiPass/ViewController.swift
+++ MultiPass/ViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+import LocalAuthentication
+import CoreData
 
 class ViewController: UIViewController {
-
+    
+    var context: NSManagedObjectContext!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector:#selector(appMovedToForeground), name: UIApplication.willEnterForegroundNotification, object: nil)
     }
-
-
+    
+    
+    
+    @objc func appMovedToForeground() {
+        let myContext = LAContext()
+        let myLocalizedReasonString = "Biometric Authntication testing !! "
+        
+        var authError: NSError?
+        if #available(iOS 8.0, macOS 10.12.1, *) {
+            if myContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &authError) {
+                myContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: myLocalizedReasonString) { success, evaluateError in
+                    
+                    DispatchQueue.main.async {
+                        if success {
+                            print("Awesome!!... User authenticated successfully")
+                        } else {
+                            print("Sorry!!... User did not authenticate successfully")
+                        }
+                    }
+                }
+            } else {
+                print("Sorry!!.. Could not evaluate policy.")
+            }
+        } else {
+            // Fallback on earlier versions
+            
+            print("Ooops!!.. This feature is not supported.")
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
 }
-
